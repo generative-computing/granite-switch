818b52e6c304d256175f13b3b76d1430b99a46eb
antonp <antonp@il.ibm.com>
2026-09-15 08:34:37 +0300

    fix linting
    Signed-off-by: antonp <antonp@il.ibm.com>

--- tests/vllm/_upstream_equivalence_tests.py
+++ tests/vllm/_upstream_equivalence_tests.py
@@ -42,7 +42,12 @@ _COMMON_CONFIG = dict(
     num_key_value_heads=2,
     num_local_experts=0,
     num_experts_per_tok=0,
-    layer_types=["full_attention", "full_attention", "full_attention", "full_attention"],
+    layer_types=[
+        "full_attention",
+        "full_attention",
+        "full_attention",
+        "full_attention",
+    ],
     hidden_act="silu",
     max_position_embeddings=512,
     attention_bias=False,
